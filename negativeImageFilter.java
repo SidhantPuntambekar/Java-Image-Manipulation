eb5746d107a1cf0e3a5fa26c7d7191dc816eedec
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-06-12 09:07:11 -0600

    Initial Commit

---
+++ negativeImageFilter.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class negativeImageFilter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class negativeImageFilter implements Filter
+{
+    public void filter(PixelImage pi)
+    {
+        Pixel[][] data = pi.getData();
+        
+        for (int row = 0; row < data.length; row ++)
+        {
+            for (int col = 0; col < data[0].length; col++)
+            {
+                Pixel p = data[row][col];
+                int red = p.getRed();
+                int green = p.getGreen();
+                int blue = p.getBlue();
+                
+                int negativeRed = 255 - red;
+                int negativeGreen = 255 - green;
+                int negativeBlue = 255 - blue;
+                
+                p.setRed(negativeRed);
+                p.setGreen(negativeGreen);
+                p.setBlue(negativeBlue);
+            }
+        }
+        
+        pi.setData(data);
+    }
+}
